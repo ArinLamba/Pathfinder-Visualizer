af95bfda5fa3e9bd78d1d09c3b9ec6db142469a1
ArinLamba <arinlamba5@gmail.com>
2025-08-21 18:37:59 +0530

    fix banner white mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { ThemeProvider } from "./components/ui/theme-provider"
+
 import { Home } from "./pages/Home"
 
 function App() {
   
   return (
-    <ThemeProvider>
+
       <Home />
-    </ThemeProvider>
     
   )
 }
