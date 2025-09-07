93a8315fbe5daa3629961320d5787ab160c8b84a
ArinLamba <arinlamba5@gmail.com>
2025-09-07 12:05:12 +0530

    fix for drag visited cell

--- src/pages/Node.tsx
+++ src/pages/Node.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ArrowRight, Target, Weight } from "lucide-react";
+import { ChevronRight, Target, Weight } from "lucide-react";
 import type { ModeSelection, NodeAttributes, Position } from "@/lib/types";
 import { cn } from "@/lib/utils";
 
@@ -34,11 +34,11 @@ export const Node = React.memo(
     else if (isEnd) className += " bg-red-600";
     else if (isWall) className += skipAnimation ? " bg-[#0D0D0D] dark:bg-[#d4d4d8]" : " dark:animate-wallCell animate-LightWallCell";
     else if (isPath) className += skipAnimation ? " bg-[#fbbf24]" : " animate-pathHighlight";
-    else if (isVisited) className += skipAnimation ? " bg-[#9333ea]" : " dark:animate-visitedCell animate-LightVisitedCell";
     else if (weight === 15) className += " animate-weightedVisitedCell dark:text-white bg-background";
     else if (isWater) className += skipAnimation ? " bg-[#1d4ed8]" : " animate-waterCell";
     else if (isGrass) className += skipAnimation ? " bg-[#15803d]" : " animate-grassCell z-50";
     else if (isMountain) className += skipAnimation ? " bg-[#8b5e3c]" : " animate-mountainCell";
+    else if (isVisited) className += skipAnimation ? " bg-[#9333ea]" : " dark:animate-visitedCell animate-LightVisitedCell";
     else className += " dark:bg-neutral-950 bg-white dark:hover:bg-neutral-800 hover:bg-white/10 transition ease-in-out";
     
     if (isDraggingStart) className += " scale-110 ring-1 ring-green-600 shadow-lg shadow-green-500/40";
@@ -55,7 +55,7 @@ export const Node = React.memo(
         onMouseEnter={onMouseEnter}
         onKeyDown={onKeyDown}
       >
-        {isStart && <ArrowRight className="aspect-square bg-yellow-500" color="black" />}
+        {isStart && <ChevronRight className="aspect-square bg-yellow-500" color="black" />}
         {isEnd && <Target className="aspect-square bg-red-600" color="black" />}
         {!isStart && !isEnd && weight === 15 && <Weight className="aspect-square" />}
       </button>
